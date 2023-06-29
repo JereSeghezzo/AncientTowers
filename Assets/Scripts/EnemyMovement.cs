391062a7a6d27ba050445381007b18195b2d7697
FerraraCiro <89488007+FerraraCiro@users.noreply.github.com>
2023-06-28 23:51:50 -0300

    First Enemy Movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+    private int targetIndex = 1;
+    public float movementSpeed = 4;
+    public float rotationSpeed = 6;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Movement();
+        LookAt();
+    }
+
+    private void Movement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[targetIndex].position, movementSpeed * Time.deltaTime);
+        var distance = Vector3.Distance(transform.position, waypoints[targetIndex].position);
+        if (distance <=0.1f)
+        {
+            if (targetIndex >= waypoints.Count - 1)
+            {
+                Debug.Log("Llegaste al final");
+                return;
+            }
+            targetIndex++;
+        }
+    }
+
+    private void LookAt()
+    {
+        var dir = waypoints[targetIndex].position - transform.position;
+        var rootTarget = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rootTarget, rotationSpeed * Time.deltaTime);
+    }
+
+}
