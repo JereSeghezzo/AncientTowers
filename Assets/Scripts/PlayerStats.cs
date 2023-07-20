c0ecd7b66940758a004b60497e081abf97e0c133
JereSeghezzo <jeremiasseghezzo@gmail.com>
2023-07-19 23:57:15 -0300

    playerstats y wavespawner

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public TowerMain towerMain;
+
+    public int TotalMoney = 50;
+
+    //public GameObject moneyText;
+
+    [SerializeField] TextMeshProUGUI moneyText;
+    [SerializeField] TextMeshProUGUI healthText;
+
+
+
+    void Start()
+    {
+        moneyText.text = "50g";
+    }
+
+
+    void Update()
+    {
+        healthText.text = towerMain.towerHealth.ToString();
+    }
+}
